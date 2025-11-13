f3db4d262d0406ea78028d6c07a8eb473e3d51dc
rorychatt <pavel.rinne@gmail.com>
2025-11-13 10:01:14 +0100

    feat: register sliplane auth provider

--- src/apps/Ivy.Open.Raise.Deploy/Program.cs
+++ src/apps/Ivy.Open.Raise.Deploy/Program.cs
@@ -1,9 +1,18 @@
 using Ivy.Open.Raise.Deploy.Apps;
+using Ivy.Sliplane.Auth;
+
 CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
 var server = new Server();
 #if DEBUG
 server.UseHotReload();
 #endif
+
+// Register Sliplane OAuth authentication provider
+server.Services.AddSliplaneAuth();
+
+// Use Sliplane auth provider
+server.UseAuth<SliplaneAuthProvider>();
+
 server.AddAppsFromAssembly();
 server.AddConnectionsFromAssembly();
 server.UseDefaultApp(typeof(DeployApp));
